8c898e3cb23b011d158465915a1d7f30fc231db4
Abdul <abdulshafey24@gmail.com>
2024-11-21 13:54:27 +0530

    Complete checkEmail functionality

---
+++ backend/controllers/checkEmail.js
@@ -0,0 +1,25 @@
+const UserModel = require("../models/userModel");
+const { ApiResponse } = require("../utils/ApiResponse");
+const { ApiError } = require("../utils/apiError");
+const { sendErrorResponse } = require("../utils/responseHelper");
+
+const checkEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (email.trim() === "") {
+      throw new ApiError(400, "Write proper Email");
+    }
+    const findEmail = await UserModel.findOne({ email }).select("-password");
+    if (!findEmail) {
+      throw new ApiError(400, "User not exist");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, findEmail, "Email verified"));
+  } catch (error) {
+    console.log("Error in checkEmail:", error);
+    return sendErrorResponse(res, error);
+  }
+};
+
+module.exports = checkEmail;
--- backend/router/index.js
+++ backend/router/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const registerUser = require("../controllers/registerUser");
+const checkEmail = require("../controllers/checkEmail");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/email", checkEmail);
 
 module.exports = router;
